1af7c4dafae1d0aba11f83c9a057bc1bdd5b65d1
Tobbie <salauoluwatobiloba0@gmail.com>
2024-05-09 13:18:42 +0100

    created contact page template

---
+++ app/contact-us/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className="overflow-x-hidden bg-lightPrimary flex min-h-screen flex-col items-center justify-between px-4 pt-8 md:p-10 gap-y-14 lg:gap-y-28
+    ">About us</div>
+  )
+}
+
+export default page
