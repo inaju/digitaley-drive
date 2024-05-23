e0eec60e1d8bcb2d9a1bd6e78c49c0f72d14951b
Salau Oluwatobiloba <103784167+Tobbiloba@users.noreply.github.com>
2024-05-23 12:30:56 +0100

    Update Works.tsx

--- app/components/Works.tsx
+++ app/components/Works.tsx
@@ -4,7 +4,7 @@ const Works = () => {
     return (
         <div className='lato flex py-10 lg:py-16 px-[1rem] flex-col items-center justify-center'>
             <p className='text-greenPrimary'>Get Discovered</p>
-            <h1 className='text-3xl lg:text-5xl text-center text-black'>Our  amazing works gets<span className='border-b-4 border-greenPrimary'>noticed by</span> </h1>
+            <h1 className='text-3xl lg:text-5xl text-center text-black'>Our  amazing work gets<span className='border-b-4 border-greenPrimary'>noticed by</span> </h1>
             <div className='flex flex-col lg:flex-row items-center gap-6 lg:gap-12 mt-20 lg:mt-12'>
                 <img src='https://superfm.online/superfmsmall.png' className='h-[6rem] w-auto' />
                 <img src='https://guardian.ng/wp-content/themes/guardian2021/img/guardian-conscience.png' className='h-[3rem] w-auto' />
